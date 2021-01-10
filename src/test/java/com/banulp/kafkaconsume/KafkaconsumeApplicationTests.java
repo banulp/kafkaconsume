b3842584662e3950d306f82e51b19855964fc5ef
banulp <banulp@gmail.com>
2021-01-10 13:01:40 +0900

    first commit

---
+++ src/test/java/com/banulp/kafkaconsume/KafkaconsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banulp.kafkaconsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaconsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
