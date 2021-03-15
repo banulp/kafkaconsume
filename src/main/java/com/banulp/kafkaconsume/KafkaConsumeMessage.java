b3352dc9c077a2103a6c4a5cb6b1cd581661cadb
banulp <banulp@gmail.com>
2021-03-16 03:58:48 +0900

    cm

---
+++ src/main/java/com/banulp/kafkaconsume/KafkaConsumeMessage.java
@@ -0,0 +1,13 @@
+package com.banulp.kafkaconsume;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumeMessage {
+
+    @KafkaListener(topics = "dg_events", groupId = "foo")
+    public void listenGroupFoo(String message) {
+        System.out.println("Received Message in group foo: " + message);
+    }
+}
