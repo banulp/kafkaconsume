216309792cf9b048752b5bd8157f68c53d153241
banulp <banulp@gmail.com>
2021-03-16 04:20:48 +0900

    topic name update

--- src/main/java/com/banulp/kafkaconsume/KafkaConsumeMessage.java
+++ src/main/java/com/banulp/kafkaconsume/KafkaConsumeMessage.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumeMessage {
 
-    @KafkaListener(topics = "dg_events", groupId = "foo")
+    @KafkaListener(topics = "dg-events", groupId = "foo")
     public void listenGroupFoo(String message) {
         System.out.println("Received Message in group foo: " + message);
     }
